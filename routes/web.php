b86bf0271c24be89276d16d5cc08d4549c08ac7e
Ivan <ivalisleo@alu.edu.gva.es>
2026-02-19 16:11:02 +0100

    CategoryRequest y CategoryController creados

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\PostController;
+    use App\Http\Controllers\CategoryController;
 
     Route::get('/', function () {
         return redirect()->route('posts.index');
     });
 
     Route::resource('posts', PostController::class);
+    Route::resource('categories', CategoryController::class);
